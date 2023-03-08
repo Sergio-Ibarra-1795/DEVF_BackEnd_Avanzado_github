d7aa2d0c46746f73baac58ab06aabbaf84d929ee
Sergibar.1795 <sergio.ibarra1795@gmail.com>
2023-03-08 14:23:49 -0600

    Agregando funcionalidad a las funciones Get y POST y agregando iddleware como ejemplo para manejo de errores_

--- Proyecto1_Back_Avanzado/server.js
+++ Proyecto1_Back_Avanzado/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+//Para importar el middleware
+const {errorHandler} = require('./middleware/errorMiddleware')
+
 //Para establecer el puerto donde la "app va a escuchar"
 const port = process.env.PORT || 5000
 
@@ -8,6 +11,9 @@ const app = express()
 
 //Para indicarle que vamos a usar json
 app.use(express.json())
+
+//Para usar el middleware
+app.use(errorHandler)
 
 
 //Para indicarle que vamos a pasar informacion con urlencode
