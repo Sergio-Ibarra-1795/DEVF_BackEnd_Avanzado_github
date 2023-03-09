25a5648173ec16b37230590aa641480dd88d5030
Sergibar.1795 <sergio.ibarra1795@gmail.com>
2023-03-08 20:16:00 -0600

    Conectando mi base de datos Proyecto1_DEVF_base1 en Mongo a nuestra primer app de express

--- Proyecto1_Back_Avanzado/controllers/tareaController.js
+++ Proyecto1_Back_Avanzado/controllers/tareaController.js
@@ -1,11 +1,14 @@
+const asyncHandler = require('express-async-handler')
+
+
 //Funcion para ver las tareas
-const getTareas = (req,res) => {
+const getTareas = asyncHandler( (req,res) => {
  res.status(200).json({mensaje:'Mostrar las tareas'})
 
-}
+})
 
 //Funcion para crear  tareas
-const setTareas = (req,res) => {
+const setTareas = asyncHandler((req,res) => {
     //Voy a asegurarme que el body de la petición tenga algo
     if(!req.body.text) {
         //res.status(400).json({message:"Favor de indicar la descripcion de la tarea"})
@@ -15,19 +18,18 @@ const setTareas = (req,res) => {
     //Si el body de la petición si tiene algo, le voy a indicar que nos indique la tarea del body
     console.log(req.body)
     res.status(201).json({mensaje:'Crear una tarea'})
-   }
+   })
   
    
 //Funcion para actualizar  tarea
-const updateTareas = (req,res) => {
+const updateTareas = asyncHandler((req,res) => {
     res.status(200).json({mensaje: `Modificar la tarea ${req.params.id}`})
-   }
+   })
 
 //Funcion para borrar  tarea
-const deleteTareas = (req,res) => {
+const deleteTareas = asyncHandler((req,res) => {
     res.status(200).json({mensaje:`Eliminar la tarea ${req.params.id}`})
-   }
-   
+   })
  
 
 module.exports = {
--- Proyecto1_Back_Avanzado/server.js
+++ Proyecto1_Back_Avanzado/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const colors = require ('colors')
 const dotenv = require('dotenv').config()
 //Para importar el middleware
 const {errorHandler} = require('./middleware/errorMiddleware')
 
+//Vamos a mandar a llamar a connectDB que nos ayuda a conectarnos a nuestra base de datos
+const connectDB = require('./config/db')
+const { connect } = require('mongoose')
+
+
 //Para establecer el puerto donde la "app va a escuchar"
 const port = process.env.PORT || 5000
+
+
+//Y vamos a conectarnos como tal a nuestra base de datos
+connectDB()
 
 //Para crear/ inicializar la app
 const app = express()
