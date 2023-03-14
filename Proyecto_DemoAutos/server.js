f03ad48786eb926481649e373d6b6c0964be4334
Sergibar.1795 <sergio.ibarra1795@gmail.com>
2023-03-13 19:06:24 -0600

    Corrigiendo el archivo de server.js dentro de Proyecto_DemoAutos para ser capaz de hacer un post de un auto con marca, anio, fecha, color, mdelo, etc

--- Proyecto_DemoAutos/server.js
+++ Proyecto_DemoAutos/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const colors = require ('colors')
 const dotenv = require('dotenv').config()
+//Vamos a mandar a llamar a connectDB que nos ayuda a conectarnos a nuestra base de datos
+const connectDB = require('./config/db')
 
 //Para importar el middleware
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -9,29 +11,20 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 //Para establecer el puerto donde la "app va a escuchar"
 const port = process.env.PORT || 5000
 
+//Y vamos a conectarnos como tal a nuestra base de datos
+connectDB()
+
 //Para crear/ inicializar la app
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //Para mandar llamar a la ruta principal que ahora se ha definido en routes
 app.use('/api/Proyecto_DemoAutos', require('./routes/autosRoutes'))
 
 
-//Para indicarle que vamos a pasar informacion con urlencode
- //Le indico que no quiero usar las 
- app.use(express.urlencoded({extended: false}))
-
-
 //Para usar el middleware
 app.use(errorHandler)
-
-
-//Vamos a mandar a llamar a connectDB que nos ayuda a conectarnos a nuestra base de datos
-const connectDB = require('./config/db')
-const { connect } = require('mongoose')
-
-
-//Y vamos a conectarnos como tal a nuestra base de datos
-connectDB()
 
 
 //Para crear la escucha en nuestro puerto
